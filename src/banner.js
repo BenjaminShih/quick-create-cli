7c49467f05f4679576c6cdab56ceda1317d785e0
sjn <shijunnan8688@dingtalk.com>
2020-12-07 14:14:44 +0800

    注释

--- src/banner.js
+++ src/banner.js
@@ -3,6 +3,7 @@ const figlet = require('figlet');
 const log = require('./logger');
 
 module.exports = function banner() {
+  // 输出LOGO
   log(
     chalk.green(
       figlet.textSync('QG---Shi Junnan', {
